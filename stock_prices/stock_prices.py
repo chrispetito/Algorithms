c255b785ca9d0766d18b69a63a4cb24a469bb75f
chrispetito <christopherpetito@gmail.com>
2019-09-11 15:28:57 -0700

    comment stock prices function

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,24 +2,29 @@
 
 import argparse
 
-p = [10, 7, 5, 8, 11, 9]
+p = [10, 7, 5, 8, 11, 900]
 p2 = [100, 90, 80, 50, 20, 10]
 
 def find_max_profit(prices):
+  # largest gain is first sell - first buy
     largest_gain = prices[1] - prices[0]
-    for i in range(len(prices) - 2):
+    # filter through prices
+    for i in range(0, len(prices) - 1):
+      # filter through range with i + 1 as starting index
       for j in range(i + 1, len(prices) - 1):
+        # if prices in j loop - prices in i loop is greater than largest_gain...
         if prices[j] - prices[i] > largest_gain:
+          # set largest_gain to that number
           largest_gain = prices[j] - prices[i]
 
     return largest_gain
 
 
-print(find_max_profit(p2))
+print(find_max_profit(p))
 
 if __name__ == '__main__':
   pass
-  This is just some code to accept inputs from the command line
+  # This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
   args = parser.parse_args()
