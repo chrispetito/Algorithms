4f7ffe8ffa5c426a4b8513cae19fb175b1824578
chrispetito <christopherpetito@gmail.com>
2019-09-11 17:06:43 -0700

    complete batches algorithm

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,26 +2,27 @@
 
 import math
 
-rec = { 'milk': 100, 'butter': 50, 'cheese': 10 }
-ingr = { 'milk': 198, 'butter': 52, 'cheese': 10 }
+rec = { 'milk': 2 }
+ingr =  { 'milk': 200}
 
 def recipe_batches(recipe, ingredients):
+  total_number_batches = 0
   # determine whether or not all ingredients are present
-  print(len(recipe))
-  recipe_array = []
-  ingredients_array = []
-  for x in recipe:
-    recipe_array.append(recipe[x])
-  for y in ingredients:
-    ingredients_array.append(ingredients[y])
-  for i in range(0, len(recipe_array)):
-    print(recipe_array[i])
+  if len(recipe) != len(ingredients):
+    return 0
+  for k in recipe and ingredients:
+    if len(recipe) == 1 and len(ingredients) == 1:
+      total_number_batches = round(ingredients[k]/recipe[k])
+    if recipe[k] > ingredients[k]:
+      total_number_batches = 0
+    if recipe[k] < ingredients[k] or recipe[k] == ingredients[k]:
+        result = ingredients[k]/recipe[k]
+        for key in recipe and ingredients:
+          if ingredients[key]/recipe[key] < result:
+            result = ingredients[key]/recipe[key]
+            total_number_batches = round(result)
+  return total_number_batches
 
-x = dict(a=1, b=2)
-y = dict(a=2, b=2)
-
-shared_items = {value: rec[value] for value in rec if value in ingr and rec[value] == ingr[value]}
-print(f'shared: {(shared_items)}')
 
 
 print(recipe_batches(rec, ingr))
